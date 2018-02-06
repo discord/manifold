030818feae4102f638cbe3976cd8608238a49920
jake <jh@discordapp.com>
2018-02-05 20:46:55 -0800

    rev to 1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Manifold.Mixfile do
   def project do
     [
       app: :manifold,
-      version: "1.2.0",
+      version: "1.3.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
