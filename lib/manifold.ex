672e964d929adeaeac1c48fec72ba3ca4b2b60e6
Sahn Lam <slam@users.noreply.github.com>
2022-09-02 23:00:36 +0000

    Code review feedback + formatting

--- lib/manifold.ex
+++ lib/manifold.ex
@@ -10,24 +10,28 @@ defmodule Manifold do
   @workers_per_partitioner Application.get_env(:manifold, :workers_per_partitioner, System.schedulers_online)
 
   @max_senders 128
-  @senders min(Application.get_env(:manifold, :senders, 1), @max_senders)
+  @senders min(Application.get_env(:manifold, :senders, System.schedulers_online), @max_senders)
 
   ## OTP
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    partitioners = for partitioner_id <- 0..(@partitioners - 1) do
-      Partitioner.child_spec(@workers_per_partitioner, [name: partitioner_for(partitioner_id)])
-    end
-    senders = for sender_id <- 0..(@senders - 1) do
-      Sender.child_spec([name: sender_for(sender_id)])
-    end
+    partitioners =
+      for partitioner_id <- 0..(@partitioners - 1) do
+        Partitioner.child_spec(@workers_per_partitioner, name: partitioner_for(partitioner_id))
+      end
 
-    Supervisor.start_link partitioners ++ senders,
+    senders =
+      for sender_id <- 0..(@senders - 1) do
+        Sender.child_spec(name: sender_for(sender_id))
+      end
+
+    Supervisor.start_link(partitioners ++ senders,
       strategy: :one_for_one,
       max_restarts: 10,
       name: __MODULE__.Supervisor
+    )
   end
 
   ## Client
@@ -35,28 +39,34 @@ defmodule Manifold do
   @spec send([pid | nil] | pid | nil, term, send_mode: :offload) :: :ok
   def send(pid, message, options \\ [])
   def send([pid], message, options), do: __MODULE__.send(pid, message, options)
+
   def send(pids, message, options) when is_list(pids) do
     case options[:send_mode] do
       :offload ->
         Sender.send(current_sender(), current_partitioner(), pids, message)
+
       nil ->
         partitioner_name = current_partitioner()
-        grouped_by = Utils.group_by(pids, fn
-          nil -> nil
-          pid -> node(pid)
-        end)
-        for {node, pids} <- grouped_by, node != nil, do: Partitioner.send({partitioner_name, node}, pids, message)
+
+        grouped_by =
+          Utils.group_by(pids, fn
+            nil -> nil
+            pid -> node(pid)
+          end)
+
+        for {node, pids} <- grouped_by,
+            node != nil,
+            do: Partitioner.send({partitioner_name, node}, pids, message)
+
         :ok
     end
   end
-  def send(pid, message, options) when is_pid(pid) do
-    case options[:send_mode] do
-      :offload ->
-        Sender.send(current_sender(), current_partitioner(), [pid], message)
-      nil ->
-        Partitioner.send({current_partitioner(), node(pid)}, [pid], message)
-      end
+
+  def send(pid, message, _options) when is_pid(pid) do
+    # Don't offload for a single pid. It doesn't really help the caller.
+    Partitioner.send({current_partitioner(), node(pid)}, [pid], message)
   end
+
   def send(nil, _message, _options), do: :ok
 
   def set_partitioner_key(key) do
@@ -93,10 +103,11 @@ defmodule Manifold do
   end
 
   def set_sender_key(key) do
-    sender = key
-    |> Utils.hash()
-    |> rem(@senders)
-    |> sender_for()
+    sender =
+      key
+      |> Utils.hash()
+      |> rem(@senders)
+      |> sender_for()
 
     Process.put(:manifold_sender, sender)
   end
@@ -105,6 +116,7 @@ defmodule Manifold do
     case Process.get(:manifold_sender) do
       nil ->
         sender_for(self())
+
       sender ->
         sender
     end
@@ -116,7 +128,7 @@ defmodule Manifold do
     |> sender_for
   end
 
-   for sender_id <- (0..@max_senders - 1) do
+  for sender_id <- 0..(@max_senders - 1) do
     def sender_for(unquote(sender_id)) do
       unquote(:"Manifold.Sender_#{sender_id}")
     end
