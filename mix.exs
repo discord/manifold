72e43bc645dbbac36a7c70e9b5fa26c57f956b49
Sahn Lam <slam@users.noreply.github.com>
2022-09-15 04:25:23 +0000

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Manifold.Mixfile do
   def project do
     [
       app: :manifold,
-      version: "1.5.0",
+      version: "1.5.1",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
